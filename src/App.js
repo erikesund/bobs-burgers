89f3072501196cdd247400b5002b8372e70b0fac
Student <eriksund84@gmail.com>
2022-08-03 18:05:20 +0100

    Added react router and movie page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TheMovie from './components/TheMovie';
 import EpContainer from './containers/EpContainer';
 import Header from './components/Header';
 
@@ -8,7 +11,12 @@ function App() {
     <div className="App">
       <Header/>
       A Work In Progress...
-      <EpContainer/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<EpContainer/>} />
+          <Route path="movie" element={<TheMovie/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/TheMovie.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TheMovie (){
+  
+  
+  return (
+    <div>This is a placeholder for the Movie page.</div>
+    )
+}
+
+export default TheMovie;
