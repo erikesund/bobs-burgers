e6df071f0d005aaaf958ddb81ef43b6fddcd03ed
Erik-Erling Sund <eriksund84@gmail.com>
2022-12-08 17:07:57 +0000

    styled blurb and poster, needs tweaking

--- src/components/movie/MovieBlurb.js
+++ src/components/movie/MovieBlurb.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "../../css/movie/MovieBlurb.css"
 
 function MovieBlurb () {
 
     return (
-        <div>
-            <p>
+        <div id="blurb-div">
+            <p id="blurb">
                 This animated, big-screen, musical comedy-mystery-adventure is based on the long- running Emmy®-winning series. The story begins when a ruptured water main creates an enormous sinkhole right in front of Bob's Burgers, blocking the entrance indefinitely and ruining the Belchers’ plans for a successful summer. While Bob and Linda struggle to keep the business afloat, the kids try to solve a mystery that could save their family's restaurant. As the dangers mount, these underdogs help each other find hope and fight to get back behind the counter, where they belong.
             </p>
         </div>
--- src/components/movie/MoviePoster.js
+++ src/components/movie/MoviePoster.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function MoviePoster () {
     return (
-        <img src="https://lumiere-a.akamaihd.net/v1/images/ino_55172057.jpeg?region=0%2C0%2C2025%2C3000" alt="Movie poster of the family singing while Bob looks like he may cry" width="40%"/>
+        <img src="https://lumiere-a.akamaihd.net/v1/images/ino_55172057.jpeg?region=0%2C0%2C2025%2C3000" alt="Movie poster of the family singing while Bob looks like he may cry" height="600pxpx"/>
     )
 }
 
